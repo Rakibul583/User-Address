3093a97ca577f0f708ba62ee39ff4c1043686614
Rakibul583 <123099697+Rakibul583@users.noreply.github.com>
2024-05-01 16:55:27 +0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,12 @@
+function TakeValues() {
+
+    var name = document.getElementById
+        ("name").value;
+
+    var password = document.getElementById
+        ("password").value;
+
+    document.write("Your Name is = " + name + "<br>");
+    document.write("Your Password is = " + password + "<br>");
+
+}
